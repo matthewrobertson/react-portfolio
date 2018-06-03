1ec0cbc48b060623f8028cb03fa3b4a6f2b27bb1
Matthew Robertson <matthewrobertson03@gmail.com>
2018-06-03 00:51:22 -0700

    fix unit tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
 import AddStockForm from './containers/AddStockContainer';
 import Holdings from './containers/HoldingsContainer';
+import store from './store';
 
 import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>
-          <AddStockForm />
-          <Holdings />
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <div>
+            <AddStockForm />
+            <Holdings />
+          </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
